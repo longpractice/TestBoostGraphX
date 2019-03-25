ae90f90b3ecdaf0ff78b26569ea7a5cf16144b15
Yang <jiyangoptics@gmail.com>
2019-03-25 23:08:06 +0100

    well tested.

---
+++ Make_De_Bruijn_Euler_Digraph.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "NamedDag.h"
+#include <Boost/timer/timer.hpp>
+#include <BoostGraphX/EulerGraph.h>
+#include <iostream>
+#include <catch.hpp>
+
+namespace bglx::test
+{
+	static std::vector<std::string> expandString(const std::vector<std::string>& names)
+	{
+		auto ch1 = '0';
+		auto ch2 = '1';
+		std::vector<std::string> expandedNames;
+		expandedNames.reserve(names.size() * 2);
+		for (auto name : names) {
+			expandedNames.emplace_back(name + ch1);
+			expandedNames.emplace_back(name + ch2);
+		}
+		return expandedNames;
+	}
+
+	static std::pair<std::string, std::string> nextName(std::string name)
+	{
+		name.erase(0, 1);
+		return { name + "0", name + "1" };
+	}
+
+	static Dag make_De_Bruigin_graph(int order)
+	{
+		auto names = std::vector<std::string>{ "0", "1" };
+		for (int i = 0; i < order; i++) {
+			names = expandString(names);
+		}
+		std::cout << names.size() << std::endl;
+		auto dag = test::Dag{};
+		std::unordered_map<std::string, test::Vertex> nameToVertex;
+		for (auto name : names) {
+			auto v = boost::add_vertex(dag);
+			dag[v].name = name;
+			nameToVertex[name] = v;
+		}
+		for (auto name : names) {
+			auto v = nameToVertex[name];
+			auto[nextName1, nextName2] = nextName(name);
+			auto vNext1 = nameToVertex[nextName1];
+			auto vNext2 = nameToVertex[nextName2];
+
+			auto eName1 = name + "0";
+			auto eName2 = name + "1";
+
+			auto[e1, ok] = boost::add_edge(v, vNext1, dag);
+			dag[e1].name = eName1;
+			auto[e2, ok2] = boost::add_edge(v, vNext2, dag);
+			dag[e2].name = eName2;
+		}
+		return dag;
+	}
+}
--- NamedDag.h
+++ NamedDag.h
@@ -1,6 +1,4 @@
 #pragma once
-#include "Timer.h"
-
 #include <BoostGraphX/Common.h>
 #include <boost/container/pmr/list.hpp>
 #include <boost/container/pmr/monotonic_buffer_resource.hpp>
@@ -15,45 +13,52 @@
 #include <string>
 #include <utility>
 #include <vector>
-namespace bglx {
-struct VertexProperty {
-    std::string name;
-};
 
-struct EdgeProperty {
-    std::string name;
-};
-
-using Dag = boost::adjacency_list<
-    boost::setS,
-    boost::vecS,
-    boost::directedS,
-    VertexProperty,
-    EdgeProperty>;
-
-using Vertex = Dag::vertex_descriptor;
-using Edge = Dag::edge_descriptor;
-using EdgeIt = Dag::edge_iterator;
-
-static void printVertex(const Dag& dag, Vertex v)
+namespace bglx::test
 {
-    std::cout << "  " << dag[v].name;
-}
+	using Dag_VSet = boost::adjacency_list<boost::setS, boost::setS, boost::directedS>;
+	
+	
+	struct VertexProperty
+	{
+		std::string name;
+	};
 
-static void printEdge(const Dag& dag, Edge edge)
-{
-    auto source = boost::source(edge, dag);
-    auto target = boost::target(edge, dag);
-    std::cout << "Edge: " << dag[source].name << " -> " << dag[target].name << std::endl;
-}
+	struct EdgeProperty
+	{
+		std::string name;
+	};
 
-static void printEdges(const Dag& dag, const std::set<Edge>& edges)
-{
-    std::cout << "--------------------------------------------" << std::endl;
-    for (auto& edge : edges) {
-        printEdge(dag, edge);
-    }
-    std::cout << "--------------------------------------------" << std::endl;
-};
+	using Dag = boost::adjacency_list<
+		boost::setS,
+		boost::vecS,
+		boost::directedS,
+		VertexProperty,
+		EdgeProperty>;
+
+	using Vertex = Dag::vertex_descriptor;
+	using Edge = Dag::edge_descriptor;
+	using EdgeIt = Dag::edge_iterator;
+
+	static void printVertex(const Dag& dag, Vertex v)
+	{
+		std::cout << "  " << dag[v].name;
+	}
+
+	static void printEdge(const Dag& dag, Edge edge)
+	{
+		auto source = boost::source(edge, dag);
+		auto target = boost::target(edge, dag);
+		std::cout << "Edge: " << dag[source].name << " -> " << dag[target].name << std::endl;
+	}
+
+	static void printEdges(const Dag& dag, const std::set<Edge>& edges)
+	{
+		std::cout << "--------------------------------------------" << std::endl;
+		for (auto& edge : edges) {
+			printEdge(dag, edge);
+		}
+		std::cout << "--------------------------------------------" << std::endl;
+	};
 
 }
