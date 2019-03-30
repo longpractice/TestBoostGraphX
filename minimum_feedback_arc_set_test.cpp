2e011ac067a7cdd2fc5c7bd3dbb9793a5e3465b2
Yang <jiyangoptics@gmail.com>
2019-03-31 00:05:08 +0100

    buildable min feedback arc set.

--- minimum_feedback_arc_set_test.cpp
+++ minimum_feedback_arc_set_test.cpp
@@ -1,5 +1,7 @@
 ﻿#include "NamedDag.h"
 #include <optional>
+#include <boost/graph/copy.hpp>
+#include <stack>
 
 namespace bglx::test
 {
@@ -14,19 +16,26 @@ namespace bglx::test
 		struct V_Prop
 		{
 			Vertex v_origin;
-			int deg_diff{};
-			//we could not put default constructed it here, since such a thing is a "singular iterator"
-			//which could not be assigned
-			//we could only have a work around to use the memory to avoid any additional memory cost
+
+			//
+			// we could not put default constructed iterator here, since such a thing is a "singular iterator"
+			// which could not be assigned
+			// we could only have a work around to use the memory to avoid any additional memory cost
+			// this is indeed just a pointer size
+			//
 			std::aligned_storage<sizeof(V_List_Pure_It), alignof(V_List_Pure_It)> v_list_it_storage;
+
 			void assign_v_list_it(V_List_Pure_It it)
 			{
-				::new ((void *)::std::addressof(v_list_it_storage)) V_List_Pure_It( it );
+				::new((void *)::std::addressof(v_list_it_storage)) V_List_Pure_It(std::move(it));
 			}
-			V_List_Pure_It v_list_it()
+
+			V_List_Pure_It& v_list_it()
 			{
 				return *reinterpret_cast<V_List_Pure_It*>((void*)std::addressof(v_list_it_storage));
 			}
+
+			int deg_diff{};
 		};
 
 		struct E_Prop
@@ -46,11 +55,11 @@ namespace bglx::test
 		struct Bin
 		{
 			V_List v_list;
-			Bin* prev_occupied_bin;
-			Bin* next_occupied_bin;
+			Bin* p_prev_occupied_bin;
+			Bin* p_next_occupied_bin;
 		};
 
-		Bin* last_occupied_bin{nullptr};
+		Bin* p_last_occupied_bin{nullptr};
 
 		using Bins = std::vector<Bin>;
 
@@ -64,7 +73,182 @@ namespace bglx::test
 
 		V_Res_List s1;
 		V_Res_List s2;
+
+		std::queue<V> unprocessed_sources;
+		std::stack<V> unprocessed_sinks;
+
 		E_Res_List feedback_edges_res;
+
+
+		void init_v_to_bin(V v)
+		{
+			auto deg_in = boost::in_degree(v, g);
+			auto deg_out = boost::out_degree(v, g);
+
+			if (deg_in == 0)
+			{
+				//source
+				unprocessed_sources.push(v);
+			}
+			else if (deg_out == 0)
+			{
+				//sink
+				unprocessed_sinks.push(v);
+			}
+			else
+			{
+				int deg_diff = static_cast<int>(deg_out) - static_cast<int>(deg_in);
+				auto& bin = get_bin(deg_diff);
+				bin.v_list.emplace_back(v);
+
+				auto& vProp = g[v];
+				vProp.deg_diff = deg_diff;
+				vProp.assign_v_list_it(std::prev(bin.v_list.end()));
+			}
+		}
+
+
+		void init_occupied_bin_list_prev()
+		{
+			Bin* current_last_occupied_bin = nullptr;
+			for (auto& bin : bins)
+			{
+				if (bin.v_list.empty())
+				{
+					continue;
+				}
+				bin.p_prev_occupied_bin = current_last_occupied_bin;
+				current_last_occupied_bin = &bin;
+			}
+			p_last_occupied_bin = current_last_occupied_bin;
+		}
+
+		void init_occupied_bin_list_next()
+		{
+			Bin* current_first_occupied_bin = nullptr;
+			for (auto itBin = bins.rbegin(); itBin != bins.rend(); ++itBin)
+			{
+				auto& bin = *itBin;
+				if (bin.v_list.empty())
+				{
+					continue;
+				}
+				bin.p_next_occupied_bin = current_first_occupied_bin;
+				current_first_occupied_bin = &bin;
+			}
+		}
+
+		void init(const Dag& g_origin)
+		{
+			auto max_in_degree = std::numeric_limits<int>::min();
+			auto max_out_degree = std::numeric_limits<int>::min();
+			auto v_copier = [&](const Vertex v_origin, const V v)
+			{
+				g[v].v_origin = v_origin;
+			};
+
+			auto e_copier = [&](const Edge& e_origin, const E& e)
+			{
+				g[e].e_origin = e_origin;
+			};
+			boost::copy_graph(g_origin, g, boost::vertex_copy(v_copier).edge_copy(e_copier));
+			for (auto v : boost::make_iterator_range(boost::vertices(g)))
+			{
+				auto in_deg = static_cast<int>(boost::in_degree(v, g));
+				auto out_deg = static_cast<int>(boost::out_degree(v, g));
+				if (in_deg > max_in_degree)
+				{
+					max_in_degree = in_deg;
+				}
+				if (out_deg > max_out_degree)
+				{
+					max_out_degree = out_deg;
+				}
+			}
+			max_out_deg = max_out_degree;
+			max_in_deg = max_in_degree;
+			bins.resize(max_in_deg + max_in_deg);
+			for (auto v : boost::make_iterator_range(boost::vertices(g)))
+			{
+				init_v_to_bin(v);
+			}
+
+			init_occupied_bin_list_prev();
+			init_occupied_bin_list_next();
+		}
+
+		void process_sources()
+		{
+			while(!unprocessed_sources.empty())
+			{
+				auto src = unprocessed_sources.back();
+				unprocessed_sources.pop();
+				for(auto tgt : boost::make_iterator_range(boost::adjacent_vertices(src, g)))
+				{
+					decrease_in_degree_on_v(tgt);
+				}
+				add_to_s1(src);
+				boost::clear_vertex(src, g);
+				boost::remove_vertex(src, g);
+			}
+		}
+
+		void process_sinks()
+		{
+			while(!unprocessed_sinks.empty())
+			{
+				auto tgt = unprocessed_sinks.top();
+				unprocessed_sinks.pop();
+				for(auto src : boost::make_iterator_range(boost::inv_adjacent_vertices(tgt, g)))
+				{
+					decrease_out_degree_on_v(src);
+				}
+				add_to_s2(tgt);
+				boost::clear_vertex(tgt, g);
+				boost::remove_vertex(tgt, g);
+			}
+		}
+
+		void process_max_delta_vertex(V v)
+		{
+			auto deg_diff = g[v].deg_diff;
+			auto& bin = get_bin(deg_diff);
+			bin.v_list.erase(g[v].v_list_it());
+			for (auto tgt : boost::make_iterator_range(boost::adjacent_vertices(v, g)))
+			{
+				decrease_in_degree_on_v(tgt);
+			}
+			for (auto src : boost::make_iterator_range(boost::inv_adjacent_vertices(v, g)))
+			{
+				decrease_out_degree_on_v(src);
+			}
+			add_to_s1(v);
+			boost::clear_vertex(v, g);
+			boost::remove_vertex(v, g);
+		}
+
+		void process()
+		{
+			if(!p_last_occupied_bin)
+			{
+				//empty graph
+				return;
+			}
+
+			while(boost::num_vertices(g) != 0)
+			{
+				while(!(unprocessed_sources.empty() && unprocessed_sinks.empty()))
+				{
+					process_sources();
+					process_sinks();
+				}
+				if(!p_last_occupied_bin)
+				{
+					auto v = p_last_occupied_bin->v_list.back();
+					process_max_delta_vertex(v);
+				}
+			}
+		}
 
 
 		int get_bin_id(int deg_diff)
@@ -93,118 +277,82 @@ namespace bglx::test
 			s2.emplace_front(g[v].v_origin);
 		}
 
-		void init_v_to_bin(V v)
-		{
-			auto deg_in = boost::in_degree(v, g);
-			auto deg_out = boost::out_degree(v, g);
-			int deg_diff = static_cast<int>(deg_out) - static_cast<int>(deg_in);
-			auto& bin = get_bin(deg_diff);
-			bin.v_list.emplace_back(v);
-
-			auto& vProp = g[v];
-			vProp.deg_diff = deg_diff;
-			vProp.assign_v_list_it(std::prev(bin.v_list.end()));
-		}
-
-		void init_occupied_bin_list_prev()
-		{
-			Bin* current_last_occupied_bin = nullptr;
-			for (auto& bin : bins)
-			{
-				if (bin.v_list.empty())
-				{
-					continue;
-				}
-				bin.prev_occupied_bin = current_last_occupied_bin;
-				current_last_occupied_bin = &bin;
-			}
-			last_occupied_bin = current_last_occupied_bin;
-		}
-
-		void int_occupied_bin_list_next()
-		{
-			Bin* current_last_occupied_bin = nullptr;
-			for (auto itBin = bins.rbegin(); itBin != bins.rend(); ++itBin)
-			{
-				auto& bin = *itBin;
-				if (bin.v_list.empty())
-				{
-					continue;
-				}
-				bin.next_occupied_bin = current_last_occupied_bin;
-				current_last_occupied_bin = &bin;
-			}
-		}
 
 		void handle_emptied_bin(Bin& bin)
 		{
 			//this bin has become empty, link the two neighbors of the linked list
-			if (bin.prev_occupied_bin)
+			if (bin.p_prev_occupied_bin)
 			{
-				bin.prev_occupied_bin->next_occupied_bin = bin.next_occupied_bin;
+				bin.p_prev_occupied_bin->p_next_occupied_bin = bin.p_next_occupied_bin;
 			}
 
-			if (bin.next_occupied_bin)
+			if (bin.p_next_occupied_bin)
 			{
-				bin.next_occupied_bin->prev_occupied_bin = bin.prev_occupied_bin;
+				bin.p_next_occupied_bin->p_prev_occupied_bin = bin.p_prev_occupied_bin;
 			}
 			else
 			{
-				last_occupied_bin = bin.prev_occupied_bin;
+				p_last_occupied_bin = bin.p_prev_occupied_bin;
 			}
 		}
 
+		//this is due to the one of the in-edges is removed
 		void handle_newly_occupied_bin_downgrade_v(Bin& bin, Bin& next_occupied_bin)
 		{
-			//From linked list A<--->B to A <---> C <---> B
-			//two tasks, 1)set the next and prev of C
-			//2) set the next of A and the prev of B
-			bin.next_occupied_bin = &next_occupied_bin;
-			bin.prev_occupied_bin = next_occupied_bin.prev_occupied_bin;
+			// From linked list A<--->B to A <---> C <---> B
+			// Two tasks:
+			//
+			// 1) set the next and prev of C
+			// 2) set the next of A and the prev of B
+			//
+			bin.p_next_occupied_bin = &next_occupied_bin;
+			bin.p_prev_occupied_bin = next_occupied_bin.p_prev_occupied_bin;
 
-			if (next_occupied_bin.prev_occupied_bin)
+			if (next_occupied_bin.p_prev_occupied_bin)
 			{
-				next_occupied_bin.prev_occupied_bin->next_occupied_bin = &bin;
+				next_occupied_bin.p_prev_occupied_bin->p_next_occupied_bin = &bin;
 			}
-			next_occupied_bin.prev_occupied_bin = &bin;
+			next_occupied_bin.p_prev_occupied_bin = &bin;
 		}
 
 		void handle_newly_occupied_bin_upgrade_v(Bin& bin, Bin& prev_occupied_bin)
 		{
-			bin.next_occupied_bin = prev_occupied_bin.next_occupied_bin;
-			bin.prev_occupied_bin = &prev_occupied_bin;
-			if (prev_occupied_bin.next_occupied_bin)
+			bin.p_next_occupied_bin = prev_occupied_bin.p_next_occupied_bin;
+			bin.p_prev_occupied_bin = &prev_occupied_bin;
+			if (prev_occupied_bin.p_next_occupied_bin)
 			{
-				prev_occupied_bin.next_occupied_bin->prev_occupied_bin = &bin;
+				prev_occupied_bin.p_next_occupied_bin->p_prev_occupied_bin = &bin;
 			}
 			else
 			{
-				last_occupied_bin = &bin;
+				p_last_occupied_bin = &bin;
 			}
-			prev_occupied_bin.next_occupied_bin = &bin;
+			prev_occupied_bin.p_next_occupied_bin = &bin;
 		}
 
 
-		//if return tree, the user got to remove the vertex from graph also
+		//if return true, the caller needs to remove the vertex from graph also
 		bool decrease_in_degree_on_v(V v)
 		{
 			auto& vProp = g[v];
 			auto old_deg_diff = vProp.deg_diff;
 			auto new_deg_diff = old_deg_diff + 1;
 			auto old_deg = boost::in_degree(v, g);
-			auto old_bin = get_bin(vProp.deg_diff);
+			auto& old_bin = get_bin(vProp.deg_diff);
 
-			//becomes a source
 			//transfer the vertex to his new home, s1 or new bin
 			if (old_deg == 1)
 			{
+				//becomes a source
 				old_bin.v_list.erase(vProp.v_list_it());
-				add_to_s1(v);
+				unprocessed_sources.push(v);
 			}
 			else
 			{
+				//upgrade to another bin
 				auto& new_bin = get_bin(new_deg_diff);
 				bool was_new_bin_empty = new_bin.v_list.empty();
+				//splice from old bin to new bin, which maintains the iterator validation
 				new_bin.v_list.splice(new_bin.v_list.end(), old_bin.v_list, vProp.v_list_it());
 				if (was_new_bin_empty)
 				{
@@ -215,26 +363,29 @@ namespace bglx::test
 			{
 				handle_emptied_bin(old_bin);
 			}
+
 			return old_deg == 1;
 		}
 
+		//if return true, the caller need to remove the vertex from the graph
 		bool decrease_out_degree_on_v(V v)
 		{
 			auto& vProp = g[v];
 			auto old_deg_diff = vProp.deg_diff;
 			auto new_deg_diff = old_deg_diff - 1;
 			auto old_deg = boost::out_degree(v, g);
-			auto old_bin = get_bin(vProp.deg_diff);
+			auto& old_bin = get_bin(vProp.deg_diff);
 
-			//becomes a source
 			//transfer the vertex to his new home, s2 or new bin
 			if (old_deg == 1)
 			{
+				//becomes a sink
 				old_bin.v_list.erase(vProp.v_list_it());
-				add_to_s2(v);
+				unprocessed_sinks.push(v);
 			}
 			else
 			{
+				//downgrade the vertex to lower bin
 				auto& new_bin = get_bin(new_deg_diff);
 				bool was_new_bin_empty = new_bin.v_list.empty();
 				new_bin.v_list.splice(new_bin.v_list.end(), old_bin.v_list, vProp.v_list_it());
